f79d533885af0461e909f2d1623d1b35aa848921
DavidV <davidv@DavidVs-MacBook-Pro.local>
2017-06-19 23:22:13 +0200

    exo finalisé hors bonus

--- index.php
+++ index.php
@@ -1,5 +1,21 @@
 <?php
-require 'functions.php';
+function lister($type) {
+	// Vérifier si les boites sont à checker ou pas
+	global $toDo;
+	$listHtml = '';
+ 	foreach ($toDo as $key => $value){
+ 		$checked = "";
+ 		if ($value['progress'] == 'done'){
+ 			$checked = "checked";
+ 		}
+ 		if ($value['progress'] == $type){
+		$item = "<li><div class='box-item'><label for='".$value['ID']."'><input name='".$value['ID']."'type='checkbox' id='" . $value['ID'] . "' value='checked'". $checked ."> " . $value['task'] . "</label></div></li> ";
+		$listHtml = $listHtml .	 $item;
+		$checked = '';
+		}
+	}
+	return "<ul>" . $listHtml . "</ul>";
+}
 ?>
 
 <!DOCTYPE html>
@@ -12,44 +28,45 @@ require 'functions.php';
 	</head>
 <body>
 		<?php
-
-		$toDo = [];
-		$done = [];
-
+		// Initier la connection à la DB
 		try {
 			$dbb = new PDO('mysql:host=localhost;dbname=todolist;charset=utf8', 'root', 'root');
 		}
 		catch (Exception $e){
 			die('Erreur : ' . $e->getMessage());
 		}
+		// Update suite à un ajout
+		if(isset($_POST['ajout'])){
+			$req = $dbb->prepare("INSERT INTO todos(task, progress) VALUES(?, 'todo')");	
+			$req->execute(array($_POST['tacheAjout']));
+			$req->closeCursor();
+		}
+		// Update suite à une tache réalisée
+		if(isset($_POST['done'])){
+			foreach ($_POST as $keyPost => $valuePost) {
+			 	if($valuePost == 'checked'){
+				$req = $dbb->prepare("UPDATE todos SET progress = 'done' WHERE ID = ? ");	
+				$req->execute(array($keyPost));
+				$req->closeCursor();
+				}
+			}
+		}
+		// Récupérer la liste et faire le tri
 		$req = $dbb->query('SELECT * FROM todos');
-
+		$toDo = [];
 		while ($donnee = $req->fetch()){
 			array_push($toDo,$donnee);
 		}
-
-
-		// Récupérer le fichier
-		//fileGetter("data.json");
-
-		// S'il y a une tâche à ajouter
-		if(isset($_POST['ajout'])){
-			modifier('ajout');
-		}
-
-		// Si une tâche a été marquée comme réalisée
-		if(isset($_POST['done'])){
-			modifier('done');
-		}
+		$req->closeCursor();
 		?>
 		<div class="container">
 			<h1>My To Do List</h1>
 			<div class="row">
 			<h2>A faire</h2>
 				<form method="post">
-				<?php
-					echo(lister($toDo, 'todo'));
-				?>
+					<?php
+					echo(lister('todo'));
+					?>
 					<input type="submit" name="done" value="done">
 				</form>
 			</div>
@@ -67,15 +84,11 @@ require 'functions.php';
 			<h2>Archive</h2>
 				<form method='post' class="crossed">
 				<?php
-					echo(lister($done, 'done'));
+				echo(lister('done'));
 				?>
 				</form>
 			</div>
-
-
 		</div>
-
-
 	<script  src="app.js"></script>
 </body>
 </html>
